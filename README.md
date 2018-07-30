313e317ce109e9429039f01c790d2ff62048b8b6
Sapphire Cat <devel@sapphirepaw.org>
2018-07-29 20:16:49 -0400

    Handle the command line better.
    Adds `-h` and `--help` options and a usage message, along with the
    actual `-a` and `--algo` options that take a hash algorithm (e.g.
    'sha384').
    
    Prints out only the one hash value, defaulting to sha256.
    
    Provides more examples in the README.
    
    Adds some gitattributes so windows and git complain less about the way I
    found the project already existing.  I may never fully understand
    core.autocrlf.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # hashsum
 File hash calculator in Python3, mainly useful for win32.
 
+Runs in Python 3.4 and up.
+
 # what?
 I decided that I _really should_ verify downloads and not trust them blindly anymore.
 I didn’t have a win32 hash calculator installed, and without one, I couldn’t verify any of them.
@@ -10,13 +12,32 @@ Luckily, I had Python already and its standard library has hash functions.
 I still haven’t downloaded a ‘real’ hash verifier.
 
 # usage
+Abstract:
 
-    [python.exe] hashsum.py FILENAME
+    [python.exe] hashsum.py [-a ALGO] FILENAME
 
-I tried using `sys.argv[1:]` but Windows decided to pass `; echo HASH` as three more filenames.
-I don’t even.
+Examples for Windows:
+
+    py -3 hashsum.py hashsum.py
+    py -3 hashsum.py -a sha256 README.md
+    py -3 hashsum.py -a md5 LICENSE
+
+Examples for Linux/OS X:
+
+    python3 hashsum.py hashsum.py
+    python3 hashsum.py -a sha256 README.md
+    python3 hashsum.py -a md5 LICENSE
+
+The `-a` option indicates the hash algorithm to use, such as “sha1”, “sha256”, or “sha384”.
+Technically, it takes any of the `hashlib` hash constructors.
+“md5” is possible, but considered weak.
 
 If you’re on Linux/OS X you may have `sha1sum`/`sha256sum` (or just `shasum`) already.
+Some examples for those commands:
+
+    sha1sum hashsum.py
+    sha256sum README.md
+    shasum -a 256 LICENSE
 
 # license
 Since this is barely even a thing: MIT.
--- hashsum.py
+++ hashsum.py
@@ -1,24 +1,38 @@
 # vim:fileencoding=utf-8
 # Python 3.4+
 
+import argparse
 import hashlib
 import sys
 from traceback import print_exc
 
-HASHES = 'sha1 sha256'.split()
-
-def hashfile (name, algo):
+def hash_file (name, algo):
     h = hashlib.new(algo)
-    try:
-        with open(name, 'rb') as f:
-            h.update(f.read())
-        return h.hexdigest()
-    except:
-        print_exc()
-        print("--" * h.digest_size)
+    with open(name, 'rb') as f:
+        h.update(f.read())
+    return h.hexdigest()
 
-def showfile (f):
-    for algo in HASHES:
-        print("{1} {2} [{0}]".format(algo, hashfile(f, algo), f))
+def create_arg_parser (**kwargs):
+    p = argparse.ArgumentParser(**kwargs)
+    p.add_argument('-a', '--algo',
+                   default='sha256',
+                   help='uses a specific hash algorithm')
+    p.add_argument('filename',
+                   help='the filename to be hashed')
+    return p
 
-showfile(sys.argv[1])
+def main (argv=None):
+    if argv is None:
+        argv = sys.argv
+    kwargs = dict(prog=argv[0],
+                  description='Displays file hashes')
+    parser = create_arg_parser(**kwargs)
+    opts = parser.parse_args(argv[1:])
+
+    # TODO: study the linux sha1sum output and format this likewise
+    hash_out = hash_file(opts.filename, opts.algo)
+    print("{0} = {1} {2}".format(hash_out, opts.algo, opts.filename))
+
+
+if __name__ == '__main__':
+    main()
